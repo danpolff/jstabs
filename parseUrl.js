bf0f6c1bd7220b18556aa651ba0744deee39e936
danpolff <57456586+danpolff@users.noreply.github.com>
2019-11-06 20:17:27 +0200

    Create parseUrl.js

---
+++ parseUrl.js
@@ -0,0 +1,7 @@
+function parseURL(url) {
+    var a =  document.createElement('a');
+    a.href = url;
+    return {
+         hash: a.hash.replace('#',''), hostname: a.hostname, pathname: a.pathname
+    };
+}
